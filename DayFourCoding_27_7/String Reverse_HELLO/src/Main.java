2aafd17878186961362324b30ea63a76e289acf4
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-08-07 13:40:02 +0400

    Add files via upload

---
+++ DayFourCoding_27_7/String Reverse_HELLO/src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+   char s[]={'h','e','l','l','o'};
+   for (int i=s.length-1;i>=0;i--){
+       System.out.print(s[i]);
+   }
+    }
+}
